4d6f6ef6bf54fc09e8d4cef5315f883ff061374c
«Evgeniya» <e.madonova@gmail.com>
2022-12-21 06:48:42 +0300

    Добавили решение

---
+++ zadacha/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] ArrayString = { "Sup", "1234", "ok",
+      "World", "Thirsday", "121", "Saturn" };
+
+string[] ArrayStringNew = new string[ArrayString.Length];
+int count=0;
+
+for (int i = 0; i < ArrayString.Length - 1; i++)
+{       if (ArrayString[i].Length<=3)
+        {
+          ArrayString[count] = ArrayString[i];
+          count++;
+        }
+}
+Array.Resize(ref ArrayString, count);
+Console.WriteLine("Всего "+count+" коротких строки");
+for (int i = 0; i < ArrayString.Length; i++)
+{
+    Console.Write("["+ArrayString[i]+"] ");
+}
+Console.WriteLine();
